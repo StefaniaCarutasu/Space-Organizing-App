0c34fb07e00ce4c49be6bd1df5207dc6c095fc8a
Stefania Carutasu <62119841+StefaniaCarutasu@users.noreply.github.com>
2021-05-10 15:21:42 +0300

    Added identity, external login using facebook

---
+++ SpaceOrganizing/Models/AppContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SpaceOrganizing.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(): base("DBConnectionString") { }
+    }
+}
