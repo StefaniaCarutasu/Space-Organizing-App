6369dfe505403446baa2156565af24708394e113
Agha Mara <62206596+maradarin@users.noreply.github.com>
2021-06-12 03:30:06 +0300

    solved bug to add task

--- SpaceOrganizing/Controllers/TasksController.cs
+++ SpaceOrganizing/Controllers/TasksController.cs
@@ -176,6 +176,7 @@ namespace SpaceOrganizing.Controllers
 
             if (IsFromGroup(userId, newTask.GroupId))
             {
+                newTask.StartDate = DateTime.Now;
                 newTask.UserId = userId;
                 newTask.User = user1;
                 newTask.Done = false;
